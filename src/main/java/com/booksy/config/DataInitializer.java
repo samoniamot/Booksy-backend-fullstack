e8a504e637508964fce7161c28235f204fb31be6
samoniamot <mainotomasmaino@gmail.com>
2025-11-27 16:45:12 -0300

    obteniendo imagened libros

--- src/main/java/com/booksy/config/DataInitializer.java
+++ src/main/java/com/booksy/config/DataInitializer.java
@@ -22,47 +22,75 @@ public class DataInitializer implements CommandLineRunner {
         if (libroRepository.count() == 0) {
             System.out.println("Inicializando datos de libros...");
             
-            // agregar algunos libros de ejemplo
+            // agregar libros clasicos con imagenes reales
             libroRepository.save(new Libro(
-                "Cien años de soledad",
-                "La novela mas famosa de Gabriel Garcia Marquez, cuenta la historia de la familia Buendia",
-                "https://images.cdn1.buscalibre.com/fit-in/360x360/85/e5/85e5e24ae498e584aa4f5e9638c681c7.jpg",
-                15990
+                "Los Hermanos Karamazov",
+                "La ultima novela de Dostoyevski, una obra maestra sobre la fe, la duda y la redencion",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788417928896_1.jpg",
+                26990
+            ));
+            
+            libroRepository.save(new Libro(
+                "Anna Karenina",
+                "Clasico de Leon Tolstoi sobre el amor prohibido y la sociedad rusa del siglo XIX",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788497945387_1.jpg",
+                17990
+            ));
+            
+            libroRepository.save(new Libro(
+                "Cuentos de Edgar Allan Poe",
+                "Relatos extraordinarios del maestro del terror y el misterio",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788497944724_1.jpg",
+                17990
+            ));
+            
+            libroRepository.save(new Libro(
+                "Cumbres Borrascosas",
+                "La apasionante historia de amor de Emily Bronte ambientada en los paramos ingleses",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788497944632_1.jpg",
+                17990
+            ));
+            
+            libroRepository.save(new Libro(
+                "H.P. Lovecraft - Antologia del Terror",
+                "Los mejores relatos cosmicos del creador de Cthulhu y los Mitos",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788497944779_1.jpg",
+                17990
             ));
             
             libroRepository.save(new Libro(
                 "Don Quijote de la Mancha",
-                "La obra maestra de Miguel de Cervantes sobre el caballero andante",
-                "https://images.cdn2.buscalibre.com/fit-in/360x360/fd/d0/fdd0e4f1d1af0e1a64989c60a4d9d6a8.jpg",
-                12500
+                "La obra cumbre de Cervantes y de la literatura universal",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788419087003_1.jpg",
+                29990
             ));
             
             libroRepository.save(new Libro(
-                "Harry Potter y la piedra filosofal",
-                "El primer libro de la saga de Harry Potter escrito por J.K. Rowling",
-                "https://images.cdn3.buscalibre.com/fit-in/360x360/c0/5f/c05f3b51e24c30d6d91f4a81f41fdea0.jpg",
-                18990
+                "Alicia en el Pais de las Maravillas",
+                "El clasico de Lewis Carroll sobre el viaje fantastico de Alicia",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788417928865_1.jpg",
+                15990
             ));
             
             libroRepository.save(new Libro(
-                "El principito",
-                "Cuento poetico de Antoine de Saint-Exupery con hermosas ilustraciones",
-                "https://images.cdn1.buscalibre.com/fit-in/360x360/04/16/041688c74c26c3c87f03db91f8d7b04c.jpg",
-                8990
+                "La Divina Comedia",
+                "El viaje de Dante por el Infierno, Purgatorio y Paraiso",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788417477677_1.jpg",
+                26990
             ));
             
             libroRepository.save(new Libro(
-                "1984",
-                "Novela distopica de George Orwell sobre un futuro totalitario",
-                "https://images.cdn2.buscalibre.com/fit-in/360x360/59/f2/59f28db44a8e3f9d7eb8f2f47d8b9c3f.jpg",
-                14500
+                "Peter Pan",
+                "La magica historia del nino que nunca crece de J.M. Barrie",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788418211522_1.jpg",
+                15990
             ));
             
             libroRepository.save(new Libro(
-                "Orgullo y prejuicio",
-                "Clasico de Jane Austen sobre el amor y las clases sociales en Inglaterra",
-                "https://images.cdn3.buscalibre.com/fit-in/360x360/a3/b4/a3b48d5e6a9c7d3f2e1b4a5c6d7e8f9a.jpg",
-                11990
+                "El Conde de Montecristo",
+                "La historia de venganza y redencion de Alejandro Dumas",
+                "https://www.antartica.cl/media/catalog/product/9/7/9788417928926_1.jpg",
+                26990
             ));
             
             System.out.println("Datos inicializados correctamente!");
